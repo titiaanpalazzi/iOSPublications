33c526204b0732f9f400fdb8029cbe3e7795450f
Titiaan Palazzi <titiaan.palazzi@gmail.com>
2015-04-27 08:38:32 -0600

    New files

--- PublicationsDuplicate/Publications-Bridging-Header.h
+++
@@ -1,6 +0,0 @@
-//
-//  Use this file to import your target's public headers that you would like to expose to Swift.
-//  This is the place where we tell Swift what Objective-C classes we want to use.
-//
-
-#import <Parse/Parse.h>
